f979a1187f019ae885d9eccfe7db9bf3a0d267ae
codefreak <harshsaid558@gmail.com>
2022-08-29 12:41:16 +0530

    code update 29-8-22

---
+++ src/Hashing/LargestConsecutiveSequence.java
@@ -0,0 +1,54 @@
+package Hashing;
+
+import java.util.*;
+
+public class LargestConsecutiveSequence {
+
+//    brute force approach with TC : o(NlogN) + o(N)
+    public static int longestConsecutiveSequence1(int[] nums) {
+        if(nums.length == 0) return 0;
+        if(nums.length == 0 && nums[0] == 0) return 1;
+        Arrays.sort(nums);
+        int size = nums.length;
+        int cnt = 1, maxi = Integer.MIN_VALUE;
+
+        for(int i = 1; i < size; i++) {
+            if(nums[i] == nums[i - 1] + 1) cnt++;
+            else if(nums[i] == nums[i - 1]) continue;
+            else cnt = 1;
+            maxi = Integer.max(maxi, cnt);
+        }
+
+        maxi = Integer.max(maxi, cnt);
+        return maxi;
+    }
+
+//    optimised approach with TC : o(N) + o(N) + o(N), SC : o(N)
+    public static int longestConsecutiveSequence(int[] nums) {
+        int size = nums.length;
+        Set<Integer> set = new HashSet<>();
+        int cnt = 0, maxi = Integer.MIN_VALUE;
+
+        for(int i = 0; i < size; i++) set.add(nums[i]);
+        for(int i = 0; i < size; i++) {
+            if(!set.contains(nums[i] - 1)) {
+                int j = nums[i];
+                while(true) {
+                    if(set.contains(j++)) cnt++;
+                    else break;
+                }
+            }
+            maxi = Integer.max(maxi, cnt);
+            cnt = 0;
+        }
+
+        return maxi;
+    }
+
+
+    public static void main(String[] args) {
+        int[] nums = {100,4,200,1,3,2};
+        System.out.println(longestConsecutiveSequence(nums));
+    }
+
+}
--- src/Hashing/_2Sum.java
+++ src/Hashing/_2Sum.java
@@ -1,7 +1,51 @@
 package Hashing;
 
-public class _2Sum {
-    public static void main(String[] args) {
+import java.util.*;
 
+public class _2Sum {
+    // this is brute force approach TC o(N ^ 2)
+    public static int[] twoSum1(int[] nums, int target) {
+        int[] ans = new int[2];
+        int size = nums.length;
+
+        for(int i = 0; i < size; i++) {
+            int newTarget = target - nums[i];
+
+            for(int j = i + 1; j < size; j++) {
+                if(nums[j] == newTarget) {
+                    ans[0] = i;
+                    ans[1] = j;
+                    break;
+                }
+            }
+        }
+
+        return ans;
+    }
+
+    // better approach using hashing TC : o(N), SC : o(N)
+    public static int[] twoSum(int[] nums, int target) {
+        int ans[] = new int[2];
+        int size = nums.length;
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for(int i = 0; i < size; i++) {
+            int newTarget = target - nums[i];
+            if(map.containsKey(newTarget)) {
+                ans[0] = map.get(newTarget);
+                ans[1] = i;
+                break;
+            }
+            map.put(nums[i], i);
+        }
+
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {2,7,11,15};
+        int target = 9;
+        int[] ans = twoSum(nums, target);
+        System.out.println(Arrays.toString(ans));
     }
 }
