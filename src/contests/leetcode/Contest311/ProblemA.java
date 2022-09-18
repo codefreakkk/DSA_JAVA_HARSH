fbec968b7880eb0f2005130a7c9b69445a70cbd2
codefreak <harshsaid558@gmail.com>
2022-09-18 10:23:46 +0530

    code update 18-9-22

---
+++ src/contests/leetcode/Contest311/ProblemA.java
@@ -0,0 +1,20 @@
+package contests.leetcode.Contest311;
+
+public class ProblemA {
+    public static int smallestEvenMultiple(int n) {
+        int ans = -1;
+        for(int i = 1; i <= 150 * 2; i++) {
+            if(i % n == 0 && i % 2 == 0) {
+                ans = i;
+                return ans;
+            }
+        }
+
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int n = 149;
+        System.out.println(smallestEvenMultiple(n));
+    }
+}
---
+++ src/contests/leetcode/Contest311/ProblemB.java
@@ -0,0 +1,28 @@
+package contests.leetcode.Contest311;
+
+import java.util.HashMap;
+
+public class ProblemB {
+    public static int longestContinuousSubstring(String s) {
+        int size = s.length();
+        int i = 0, j = 1, max = 1;
+
+        while(i < size && j < size) {
+            if(s.charAt(j) - s.charAt(j - 1) == 1) {
+                max = Math.max(j - i + 1, max);
+                j++;
+            }
+            else {
+                i = j;
+                j++;
+            }
+        }
+
+        return max;
+    }
+
+    public static void main(String[] args) {
+        String s = "yrpjofyzubfsiypfre";
+        System.out.println(longestContinuousSubstring(s));
+    }
+}
